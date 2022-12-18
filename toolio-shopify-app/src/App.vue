5f7c6151c4265094c291e177fecc8b33ad9619b6
Misranur Yazgan <yazganmisra@gmail.com>
2022-12-18 20:49:41 +0300

    Home view and error alert are added.

--- toolio-shopify-app/src/App.vue
+++ toolio-shopify-app/src/App.vue
@@ -14,21 +14,8 @@
     </v-app-bar>
 
     <v-main>
-      <v-container fluid class="mt-6">
-        <v-row >
-          <v-col cols="12" md="6" offset-md="3">
-            <search-bar @searchProduct="getProducts($event)" />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="6" offset-md="3">
-            <products-table :products="this.products" 
-              :noProductsFound="noProductsFound" 
-              :isLoading="isLoading" 
-              :isSearchStarted="isSearchStarted"
-            />
-          </v-col>
-        </v-row>
+      <v-container>
+        <router-view></router-view>
       </v-container>
     </v-main>
     
@@ -36,57 +23,7 @@
 </template>
 
 <script>
-  import SearchBar from '@/components/SearchBar.vue';
-  import ProductsTable from '@/components/ProductsTable.vue'
-  
   export default {
-    name: "App",
-
-    components: {
-      SearchBar,
-      ProductsTable
-    },
-
-    data () {
-      return {
-        products: [],
-        noProductsFound: false,
-        isLoading: false,
-        isSearchStarted: false
-      }
-    },
-
-    methods: {
-      async getProducts(titleToSearch) {
-        this.products = [];
-        this.noProductsFound = false;
-        this.isLoading = true;
-        this.isSearchStarted = true;
-
-        const PORT = 3000;
-        const API_URL = "http://localhost:" + PORT + "/products?title=" + titleToSearch;
-        console.log("url", API_URL);
-  
-        fetch(API_URL)
-          .then(response => {
-            console.log("statusText", response.statusText);
-            return response.json();
-          })
-          .then(products => {
-            console.log("products:", products);
-            this.isLoading = false;
-
-            if (products.products != null)
-            {
-              this.products = products.products;
-            }
-            else
-            {
-              this.noProductsFound = true;
-            }
-          })
-          .catch(error => console.log(error));
-      }
-    }
+    name: "App"
   }
 </script>
--- toolio-shopify-app/src/views/HomeView.vue
+++ toolio-shopify-app/src/views/HomeView.vue
@@ -1,15 +1,87 @@
 <template>
-  <hello-world />
+  <v-container fluid class="mt-6">
+    <v-row >
+      <v-col cols="12" md="8" offset-md="2">
+        <search-bar @searchProduct="getProducts($event)" />
+      </v-col>
+    </v-row>
+    <v-row v-if="displayError">
+      <v-col cols="12" md="8" offset-md="2">
+        <v-alert dense outlined dismissible type="error">
+          Oops! Something went wrong.
+        </v-alert>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="8" offset-md="2">
+        <products-table :products="this.products" 
+          :noProductsFound="noProductsFound" 
+          :isLoading="isLoading" 
+          :isSearchStarted="isSearchStarted"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import SearchBar from '@/components/SearchBar.vue';
+  import ProductsTable from '@/components/ProductsTable.vue'
 
   export default {
-    name: 'HomePage',
+    name: "HomeView",
 
     components: {
-      HelloWorld,
+      SearchBar,
+      ProductsTable
     },
+
+    data () {
+      return {
+        products: [],
+        noProductsFound: false,
+        isLoading: false,
+        isSearchStarted: false,
+        displayError: false
+      }
+    },
+
+    methods: {
+      async getProducts(titleToSearch) {
+        this.products = [];
+        this.noProductsFound = false;
+        this.isLoading = true;
+        this.isSearchStarted = true;
+        this.displayError = false;
+
+        const PORT = 3000;
+        const API_URL = "http://localhost:" + PORT + "/products?title=" + titleToSearch;
+        console.log("url", API_URL);
+  
+        fetch(API_URL)
+          .then(response => {
+            console.log("statusText", response.statusText);
+            return response.json();
+          })
+          .then(products => {
+            console.log("products:", products);
+            this.isLoading = false;
+
+            if (products.products != null)
+            {
+              this.products = products.products;
+            }
+            else
+            {
+              this.noProductsFound = true;
+            }
+          })
+          .catch(error => {
+            this.displayError = true;
+            this.isLoading = false;
+            console.log(error);
+          });
+      }
+    }
   }
 </script>
